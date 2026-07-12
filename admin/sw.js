3d01c89a4f39c7ea93d035a4bb70d407a3e2e26f
RAFIQUL ISLAM <126047983+Sanvi009@users.noreply.github.com>
2026-07-12 13:36:13 +0600

    Update sw.js

--- admin/sw.js
+++ admin/sw.js
@@ -17,7 +17,20 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) return response;
+        
+        return fetch(event.request).then(networkResponse => {
+          if (networkResponse && networkResponse.status === 200) {
+            const responseClone = networkResponse.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseClone);
+            });
+          }
+          return networkResponse;
+        });
+      })
+      .catch(() => caches.match('/admin/'))
   );
 });
 
